cb23944b088b99ea2497c5805cfe345c8cfcc2fc
manvirk21 <manvirkaur2113@gmail.com>
2024-12-10 21:12:51 -0600

    added comments

--- m3.py
+++ m3.py
@@ -5,7 +5,21 @@ import random, sys
 
 class M3:
     def __init__(self, grammar:Grammar, specification:Specification, name:str="my_func", verbose:bool=False):
-        """Initializes an instance of M3 to synthesize a program based on the given grammar and specification"""
+        """
+        This function initializes an instance of M3 to syntesize a program based on the specified grammar and
+        specification.
+        
+        :param grammar: The `grammar` parameter is expected to be an object of the `Grammar` class. It is
+        used to define the grammar rules or structure that will be used in the functionality of the class or
+        method where this `__init__` method is defined
+        :param name: The `name` parameter in the `__init__` method is a string that represents the name of
+        the function being initialized. In this case, the default value for the `name` parameter is
+        "my_func", defaults to my_func
+        :param verbose: The `verbose` parameter is a boolean flag that indicates whether additional
+        information or details should be displayed during the execution of the function. If `verbose` is set
+        to `True`, the function will print out more information to help with debugging or understanding the
+        process. If `verbose` is set to `, defaults to False
+        """
         self.grammar = grammar
         self.specification = specification
         self.name = name
@@ -14,14 +28,29 @@ class M3:
 
     def synthesize(self, max_synth_iter:int=None, max_verify_checks:int=500) -> Callable:
         """
-        Synthesize a function that makes the spepcification true when it is substituted into the specification
+        This function `synthesize` takes in two optional parameters `max_synth_iter` and
+        `max_verify_checks`
+        Synthesizes a function that makes the spepcification true when it is substituted into the specification
         
         Returns:
             Callable: the synthesized function
+
+        :param max_synth_iter: The `max_synth_iter` parameter specifies the maximum number of synthesis
+        iterations that can be performed during the synthesis process. If this parameter is not
+        provided, it defaults to `None`, meaning there is no specific limit set on the number of
+        synthesis iterations
+        :param max_verify_checks: The `max_verify_checks` parameter specifies the maximum number of
+        verification checks that can be performed during the synthesis process. This parameter helps
+        control the computational resources used during the synthesis operation. By setting a limit on
+        the number of verification checks, you can prevent the synthesis process from running indefinitely 
+        or consuming excessive resources, defaults to 500
         """
+ 
         self.i = 0
+        # Check if a maximum number iterations is set and if the current iteration count exceeds it. If the
+        # maximum is reached, it prints a message indicating the max iterations have been reached and 
+        # breaks out of the loop.
         while True:
-            # Check iterations
             self.i += 1
             if max_synth_iter and self.i > max_synth_iter:
                 if self.verbose: print(f"!!! Max iterations ({max_synth_iter}) reached, stopping synthesis")
@@ -38,6 +67,9 @@ class M3:
 
             # Term Solver - generates terms until all points are covered
             if self.verbose: print("\tTerm Solver:")
+            # Checking if the result of the `_cover_union()` method is not equal to `self.pts`. Within the 
+            # loop, the code is calling the `_next_distinct_term()` method and adding the result to the 
+            # `self.terms` set.
             while self._cover_union() != self.pts:
                 term = self._next_distinct_term()
                 self.terms.add(term)
@@ -46,15 +78,12 @@ class M3:
             # Unifier - generates predicates and adds additional terms repeatedly to 
             # try to learn a decision tree such that all points are covered
             if self.verbose: print(f"\tUnifier:")
-            #k = 0
             while self.decision_tree is None:
                 # add term
-                #if k>0 or self.i==1:
                 term = self._next_distinct_term()
                 if term: self.terms.add(term)
                 if self.verbose and term: print(f"\t\tAdded term {term}, now generating predicates: ", end="")
                 elif self.verbose: print(f"\t\tGenerating predicates: ", end="")
-                #k += 1
                 
                 # generate predicates
                 self.preds = self.grammar.enumerate_predicates(self.terms)
@@ -87,33 +116,70 @@ class M3:
             if self.verbose: print(f"\t\tCounter-example found: {cexpt}")
             
     def _cover_union(self) -> set[str]:
-        """Returns the set of all pts covered by terms in self.terms"""
+        """
+        This function returns the set of all points covered by terms in the `self.terms` attribute.
+        :return: The function `_cover_union` returns a set of all points covered by terms in the
+        `self.terms` attribute.
+        """
         return set([pt for cover_i in self.cover.values() for pt in cover_i])
         
     def _decision_tree_to_expr(self) -> str:
-        """Returns decision tree as a pythonic expression"""
+        """
+        The function `_decision_tree_to_expr` returns the decision tree as a Pythonic expression.
+        :return: The method `_decision_tree_to_expr` returns the decision tree as a pythonic expression
+        by calling the `__repr__` method on the `decision_tree` object.
+        """
         return self.decision_tree.__repr__()
     
     def _expr_to_func_callable(self, expr:str) -> Callable:
-        """"""
+        """
+        The function `_expr_to_func_callable` is not implemented and raises a `NotImplementedError`.
+        
+        :param expr: `_expr_to_func_callable` that takes an expression as a string input. The function is not 
+        implemented and raises a `NotImplementedError`.
+        :type expr: str
+        """
         raise NotImplementedError("_expr_to_func_callable()")
     
     def _expr_to_func_str(self, expr:str) -> str:
-        """Adds function signature based on grammar to the given expression"""
+        """
+        The function `_expr_to_func_str` adds a function signature based on grammar to a given expression.
+        
+        :param expr: The `expr` parameter is a string representing an expression that you want to convert into 
+        a function
+        :type expr: str
+        :return: The function `_expr_to_func_str` returns a string that includes the function signature based 
+        on grammar followed by the given expression. The function signature includes the function name and 
+        identifiers from the grammar. The returned string is formatted with proper indentation based on the 
+        verbose setting.
+        """
         offset = "\t\t\t" if self.verbose else "   "
         func_str = f"{offset}def {self.name}({self.grammar.identifiers()}): \n{expr}"
 
         return func_str.replace("\n", f"\n{offset}")
 
     def _generate_test_pts(self, args_num:int) -> Generator:
-        """Generates random test points infinitely"""
+        """
+        The function generates random test points infinitely within specified ranges and steps.
+        
+        :param args_num: The `args_num` parameter in the `_generate_test_pts` method represents the number of 
+        arguments to be generated for each test point. This value determines the length of the list `pt` that 
+        is generated with random integer values within the specified range :type args_num: int
+        """
         range_max = sys.maxsize
         current_range_max = 10
         range_step = 10
         pts_generated = 0
  
         def comlement_min(max:int) -> int:
-            """Corresponding min integer for a given max integer"""
+            """
+            The function returns the corresponding minimum integer for a given maximum integer.
+            
+            :param max: The `max` parameter in the `complement_min` function represents the maximum integer 
+            value for which you want to find the corresponding minimum integer value
+            :type max: int
+            :return: the corresponding minimum integer for a given maximum integer by calculating -max - 1.
+            """
             return -max - 1
 
         while True:
@@ -132,7 +198,13 @@ class M3:
                 range_step *= 2
     
     def _learn_decision_tree(self) -> Union[DecisionTree, None]:
-        """Learn a decision tree based on the current pts, terms, cover and preds"""
+        """
+        The function recursively learns a decision tree based on given data points, terms, cover, and predicates.
+        :return: The function `_learn_decision_tree` is returning a learned decision tree based on the current 
+        points, terms, cover, and predicates. It first defines a helper function `learn_dt` to recursively 
+        learn the decision tree. The function then calls `learn_dt` with the initial parameters and returns the 
+        resulting decision tree.
+        """
         # recursive helper function to learn dt
         def learn_dt(pts:set, terms:set, cover:dict, preds:list[str]):
             # check if all points are covered by a single term
@@ -162,7 +234,12 @@ class M3:
         return DecisionTree(root, self.grammar.identifiers(as_list=True))
 
     def _next_distinct_term(self) -> str:
-        """Returns the next term that covers a set of points not covered by a term already in self.terms"""
+        """
+        The function iterates through candidate terms, checking if they cover a set of points not already 
+        covered by existing terms, and saves equivalent terms if their covers are not distinct.
+        :return: The `_next_distinct_term` method returns the next term that covers a set of points not covered 
+        by a term already in `self.terms`.
+        """
         if self.verbose: print(f"\t\tPts={self.pts if self.pts else '{}'}, Cover Pts={[item for item in self.cover.items()] if self.cover else '{}'}")
         
         while True:
